45efb0440cefb2f439d1da6572933595e50347c4
Ilya Rim <Rimmer202@mail.ru>
2026-04-21 21:58:54 +0300

    fix: исправление ошибок в UI тестах

--- tests/ui/HomeWorkLearn5.spec.js
+++ tests/ui/HomeWorkLearn5.spec.js
@@ -1,97 +1,140 @@
-import { faker } from '@faker-js/faker';
 import { test, expect } from '../../src/helpers/fixtures/fixture.js';
 import { UserBuilder, ArticleBuilder } from '../../src/helpers/builders/index.builder';
 
+test.describe('HomeWorkLearn5 - Tests with isolated data', () => {
+  let user;
+  let email;
+  let password;
+  let name;
+  let articleData;
+  let title;
+  let description;
+  let article;
+  let tags;
 
-const user = new UserBuilder().withEmail().withName().withPassword().build();
-const {email, password, name} = user;
-const articleData = new ArticleBuilder().withTitle().withDescription().withArticle().withTags().build();
-const { title, description, article, tags } = articleData;
+  //Создаем свежие данные перед КАЖДЫМ тестом
+  test.beforeEach(async () => {
+    user = new UserBuilder().withEmail().withName().withPassword().build();
+    email = user.email;
+    password = user.password;
+    name = user.name;
+    
+    articleData = new ArticleBuilder().withTitle().withDescription().withArticle().withTags().build();
+    title = articleData.title;
+    description = articleData.description;
+    article = articleData.article;
+    tags = articleData.tags;
+  });
 
-test('Регистрация, авторизация и разлогин', async ({ app }) => {
-  
-  await app.homePage.goToRegister();
-  await app.registrationPage.registration(name, email, password); // Регистрация
-  
-  await app.profilePage.openMenu();
-  await app.profilePage.logOut(); //Выход после регистрации
-  
-  await app.homePage.goToLogin();
-  await app.loginPage.login(email, password); // Авторизация
-  
-  await expect(app.homePage.profileName).toContainText(name); // Проверяем, что пользователь авторизовался
-  
-  await app.profilePage.openMenu();
-  await app.profilePage.logOut();
-  await expect(app.homePage.mainContentnav).toContainText('Sign up'); // Выход после авторизации
-}); 
+  test('Регистрация, авторизация и разлогин', async ({ app }) => {
+    await app.homePage.goToRegister();
+    await app.registrationPage.registration(name, email, password);
+    
+    await app.profilePage.openMenu();
+    await app.profilePage.logOut();
+    
+    await app.homePage.goToLogin();
+    await app.loginPage.login(email, password);
+    
+    await expect(app.homePage.profileName).toContainText(name);
+    
+    await app.profilePage.openMenu();
+    await app.profilePage.logOut();
+    await expect(app.homePage.mainContentnav).toContainText('Sign up');
+  }); 
 
-test('Применение фильтра', async ({ app } ) => {
-
+  test('Применение фильтра', async ({ app }) => {
     await app.homePage.goToRegister();
     await app.registrationPage.registration(name, email, password);
     await expect(app.homePage.profileName).toContainText(name); 
-
     
     await app.profilePage.filterSelect();
-    await expect(app.profilePage.filter).toBeVisible(); // Фильтр применен
-});
+    await expect(app.profilePage.filter).toBeVisible();
+  });
 
-test('Создание статьи', async ({ app }) => {
-
+  test('Создание статьи', async ({ app }) => {
     await app.homePage.goToRegister();
     await app.registrationPage.registration(name, email, password);
-    await app.homePage.goToArticle()
+    await app.homePage.goToArticle();
     await app.articlePage.addArticle(title, description, article, tags);
     await app.profilePage.openMenu();
     await app.profilePage.goToProfile();
-    await expect(app.profilePage.title).toContainText(description); // Успешно добавили cтатью
+    await expect(app.profilePage.title).toContainText(title);
+  });
 
-});
-
-test('Удаление статьи', async ({ app } ) => {
-    
+  test('Удаление статьи', async ({ app }) => {
     await app.homePage.goToRegister();
     await app.registrationPage.registration(name, email, password);
-
-    await app.homePage.goToArticle()
+    await app.homePage.goToArticle();
     await app.articlePage.addArticle(title, description, article, tags);
-
     await app.profilePage.openMenu();
     await app.profilePage.goToProfile();
     await app.profilePage.openArticle();
     
     await Promise.all([
-    app.page.waitForEvent('dialog').then(dialog => {
-      expect(dialog.message()).toBe('Want to delete the article?');
-      dialog.accept();
-    }),
-    app.openArticlePage.deleteArticle()
-  ]);
+      app.page.waitForEvent('dialog').then(dialog => {
+        expect(dialog.message()).toBe('Want to delete the article?');
+        dialog.accept();
+      }),
+      app.openArticlePage.deleteArticle()
+    ]);
     
-    await expect(app.profilePage.noArticles).toBeVisible(); // Успешно удалили cтатью
-  }
-);
+    await expect(app.profilePage.noArticles).toBeVisible();
+  });
 
-test('Добавление комментария к статье', async ({ app }) => {
-    
-    
+  test('Добавление комментария к статье', async ({ app }) => {
     const commentData = new ArticleBuilder().withComment().buildComment();
     const comment = commentData.text;
     
     await app.homePage.goToRegister();
     await app.registrationPage.registration(name, email, password);
-    await app.homePage.goToArticle()
+    await app.homePage.goToArticle();
     await app.articlePage.addArticle(title, description, article, tags);
     await app.profilePage.openMenu();
     await app.profilePage.goToProfile();
     await app.profilePage.openArticle();
-    // Добавляем комментарий к статье
+    
     await app.comment.addComment(comment);
-
+    
     const commentDataResult = await app.comment.commentData();
-    // Проверяем, что комментарий добавлен
     expect(commentDataResult).not.toBeNull();
     expect(commentDataResult.text).toContain(comment);
     expect(commentDataResult.author).toContain(user.name);
-    })
+  });
+
+  test('Редактирование статьи', async ({ app }) => {
+    const updatedArticleData = new ArticleBuilder().withTitle().withDescription().withArticle().withTags().build();
+    const { title: updatedTitle, description: updatedDescription, article: updatedArticle, tags: updatedTags } = updatedArticleData;
+  
+    await app.homePage.goToRegister();
+    await app.registrationPage.registration(name, email, password);
+    await app.homePage.goToArticle();
+    await app.articlePage.addArticle(title, description, article, tags);
+    await app.profilePage.openMenu();
+    await app.profilePage.goToProfile();
+    await app.profilePage.openArticle();
+    await app.openArticlePage.editArticle();
+  
+    await app.articlePage.titleInput.fill(updatedTitle);
+    await app.articlePage.aboutInput.fill(updatedDescription);
+    await app.articlePage.articleInput.fill(updatedArticle);
+    await app.articlePage.tagsInput.fill(updatedTags);
+    await app.page.getByRole('button', { name: 'Update Article' }).click();
+  
+    await expect(app.page.getByRole('heading')).toContainText(updatedTitle);
+    await expect(app.page.locator('.article-content p').first()).toContainText(updatedArticle);
+  });
+
+  test('Применение тега и поиск статьи по тегу', async ({ app }) => {
+    const searchTag = 'реклама';
+  
+    
+    await app.homePage.goToRegister();
+    await app.registrationPage.registration(name, email, password);
+    await app.page.getByRole('button', { name: searchTag }).click();
+    const firstArticle = app.page.getByRole('link').first();
+    await firstArticle.click();
+    await expect(app.page.locator('.tag-list')).toContainText(searchTag);
+  });
+  
+});
